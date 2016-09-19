e627410ebe28701a08726fb6bbe4b33bcc0a647f
Stephen Salyer <ssalyer1@dtcc.edu>
2016-09-18 23:38:04 -0400

    The other stuff

--- IceCreamListsPart2/Form1.Designer.cs
+++ IceCreamListsPart2/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbxOrders = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxOrders
+            // 
+            this.lbxOrders.FormattingEnabled = true;
+            this.lbxOrders.Location = new System.Drawing.Point(0, 0);
+            this.lbxOrders.Name = "lbxOrders";
+            this.lbxOrders.Size = new System.Drawing.Size(283, 199);
+            this.lbxOrders.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 205);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 205);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lbxOrders);
+            this.Name = "Form1";
+            this.Text = "Cones \'R Us";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbxOrders;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- IceCreamListsPart2/Form1.cs
+++ IceCreamListsPart2/Form1.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace IceCreamListsPart2
 {
@@ -15,6 +13,52 @@ namespace IceCreamListsPart2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you'd like to clear the file?", "Cones 'R Us",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // Erase contents of file
+                FileStream clearFile = File.OpenWrite("orders.txt");
+                clearFile.SetLength(0);
+
+                // Clear the form
+                lbxOrders.Items.Clear();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string order;
+
+            try
+            {
+                // Open the orders.txt file
+                StreamReader orderFile = File.OpenText("orders.txt");
+
+                while (!orderFile.EndOfStream)
+                {
+                    // Display the contents of the file
+                    order = orderFile.ReadLine();
+                    lbxOrders.Items.Add(order);
+                }
+
+                orderFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Cones 'R Us",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
         }
     }
 }
